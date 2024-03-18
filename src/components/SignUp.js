31a7feddac0e5b413caa512ca42de10f70f0fa0b
Smriti <smriti12sci.nvm@gmail.com>
2024-03-19 01:16:07 +0530

    signup styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,33 +6,33 @@ function SignUp(){
         <div className='font-bold text-4xl'>
         <h1>Sign Up</h1>
         </div>
-            <form className="mt-5">
-            <div className='flex justify-center'>
-                <div className="Username">
+            <form className="mt-10">
+            <div className='flex justify-center mt-5'>
+                <div className='text-2xl '>
                     <label htmlFor="username">Username:</label>
                 </div>
                 <div className="Username-input">
-                    <input type="text" id="username" name="username" />
+                    <input className='border-2 rounded-md' type="text" id="username" name="username" />
                 </div>
             </div>
-            <div className="flex justify-center">
-                <div className="Email">
+            <div className="flex justify-center  mt-5">
+                <div className="text-2xl">
                     <label htmlFor="email">Email:</label>
                 </div>
                 <div className="Email-input">
-                    <input type="text" id="email" name="email" />
+                    <input className='border-2 rounded-md' type="text" id="email" name="email" />
                 </div>
             </div>
-            <div className="flex justify-center">
-                <div className="Password">
+            <div className="flex justify-center mt-5">
+                <div className="text-2xl ">
                     <label htmlFor="password">Password:</label>
                 </div>
                 <div className="Password-input">
-                    <input type="password" id="password" name="password" />
+                    <input className='border-2 rounded-md' type="password" id="password" name="password" />
                 </div>
             </div>
-            <div className="submit-button">
-                <button type="submit">Sign Up</button>
+            <div className="text-2xl mt-10">
+                <button className='btn' type="submit">Sign Up</button>
             </div>
             </form>
             </div>
