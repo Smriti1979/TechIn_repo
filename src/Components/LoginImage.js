cc61850051bfdd973e8be2fdce028d0619f2fdab
Smriti <smriti12sci.nvm@gmail.com>
2024-03-24 13:36:00 +0530

    image added

---
+++ src/Components/LoginImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LImage from '../Images/Limage.png';
+
+function LoginImage() {
+  return (
+    <div>
+        <img src={LImage}></img>
+    </div>
+  )
+}
+
+export default LoginImage;
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,49 +1,27 @@
 import React from 'react';
+import LoginImage from '../Components/LoginImage';
 
 const Page2Content = () => {
   return (
     <div className="grid grid-cols-2">
       {/* Picture */}
       <div className="col-span-1 h-screen">
-        <img
-          src="your-image-url.jpg"
-          alt="Your Image"
-          className="w-full h-full object-cover"
-        />
+        <LoginImage/>
       </div>
 
       {/* Content Block */}
       <div className="col-span-1 h-screen bg-black flex justify-center items-center">
-        <div className="bg-white p-16 rounded-lg">
-          <h2 className="text-4xl font-bold text-black mb-8">LOG IN</h2>
-          <div className="relative mb-8">
-            <input
-              type="text"
-              placeholder="Username"
-              className="w-full py-3 px-4 border border-gradient rounded-md focus:outline-none focus:border-blue-500"
-            />
-            <div className="absolute top-0 left-0 w-full h-full border-gradient border border-solid rounded-md pointer-events-none"></div>
-          </div>
-          <div className="relative mb-8">
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full py-3 px-4 border border-gradient rounded-md focus:outline-none focus:border-blue-500"
-            />
-            <div className="absolute top-0 left-0 w-full h-full border-gradient border border-solid rounded-md pointer-events-none"></div>
-          </div>
-          <div>
-              <div className="flex items-center justify-between mb-8">
-                <p className="text-gray-500">OR</p>
-              </div>
-              <div className="flex items-center justify-between mb-8">
-                <div className="flex items-center space-x-4">
-                  <img src="/google-icon.png" alt="Google" className="h-10 w-10" />
-                  <img src="/twitter-icon.png" alt="Twitter" className="h-10 w-10" />
-                  <img src="/facebook-icon.png" alt="Facebook" className="h-10 w-10" />
-                </div>
-              </div>
-          </div>
+        <div className='bg-white p-20 rounded-md px-64 pt-40 pb-60 rounded-lg'>
+          <h1 className='text-4xl font-bold text-center'>Sign Up</h1>
+          <form className='flex flex-col mt-10'>
+            <input className='border-b-2 border-black mb-5' type='text' placeholder='First Name'></input>
+            <input className='border-b-2 border-black mb-5' type='text' placeholder='Last Name'></input>
+            <input className='border-b-2 border-black mb-5' type='email' placeholder='Email'></input>
+            <input className='border-b-2 border-black mb-5' type='password' placeholder='Password'></input>
+            <input className='border-b-2 border-black mb-5' type='password' placeholder='Confirm Password'></input>
+            <button className='bg-black text-white rounded-md p-2 mt-5'>Sign Up</button>
+          </form>
+
         </div>
       </div>
     </div>
