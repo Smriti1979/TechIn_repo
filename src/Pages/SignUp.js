8535adfe744847e323fa9b048bafca53150b2be5
Smriti <smriti12sci.nvm@gmail.com>
2024-03-24 15:07:52 +0530

    login page changes

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -10,18 +10,39 @@ const Page2Content = () => {
       </div>
 
       {/* Content Block */}
-      <div className="col-span-1 h-screen bg-black flex justify-center items-center">
-        <div className='bg-white p-20 rounded-md px-64 pt-40 pb-60 rounded-lg'>
+      <div className=" h-screen bg-black flex justify-center items-center">
+        <div className='bg-white rounded-md px-60 pt-40 pb-40 rounded-lg'>
           <h1 className='text-4xl font-bold text-center'>Sign Up</h1>
-          <form className='flex flex-col mt-10'>
-            <input className='border-b-2 border-black mb-5' type='text' placeholder='First Name'></input>
-            <input className='border-b-2 border-black mb-5' type='text' placeholder='Last Name'></input>
-            <input className='border-b-2 border-black mb-5' type='email' placeholder='Email'></input>
-            <input className='border-b-2 border-black mb-5' type='password' placeholder='Password'></input>
-            <input className='border-b-2 border-black mb-5' type='password' placeholder='Confirm Password'></input>
-            <button className='bg-black text-white rounded-md p-2 mt-5'>Sign Up</button>
-          </form>
-
+          <div className='w-80'>
+              <form className='flex flex-col mt-10'>
+              <input
+              type="text"
+              placeholder="First Name"
+              className="w-full py-2 px-4 border border-gradient rounded-md focus:outline-none focus:border-black-500"
+              />
+              <input
+              type="text"
+              placeholder="Last Name"
+              className="w-full py-2 px-4 border border-gradient rounded-md focus:outline-none focus:border-black-500"
+              />
+              <input
+              type="email"
+              placeholder="Email"
+              className="w-full py-2 px-4 border border-gradient rounded-md focus:outline-none focus:border-black-500"
+              />
+              <input
+              type="password"
+              placeholder="Passwor"
+              className="w-full py-2 px-4 border border-gradient rounded-md focus:outline-none focus:border-black-500"
+              />
+              <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-full py-2 px-4 border border-gradient rounded-md focus:outline-none focus:border-black-500"
+              />
+              <button className='text-white rounded-md p-2 mt-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Sign Up</button>
+              </form>
+          </div>
         </div>
       </div>
     </div>
